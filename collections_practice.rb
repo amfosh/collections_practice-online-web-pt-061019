121ca8bc5b0cafa08f5ccaf6487cde6b4105ebf9
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-20 22:03:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
+def sort_array_asc(array)
+  array.sort 
+end
 
+def sort_array_desc(array)
+  array.sort { |a,z| z <=> a } 
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
